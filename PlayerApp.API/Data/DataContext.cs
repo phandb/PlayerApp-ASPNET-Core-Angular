188b80686c595f031eb3e9f697b961e27b8105d4
Dalton <phandb72@gmail.com>
2019-07-05 18:04:59 -0500

    Add Security feature.  Create User model

--- PlayerApp.API/Data/DataContext.cs
+++ PlayerApp.API/Data/DataContext.cs
@@ -8,5 +8,7 @@ namespace PlayerApp.API.Data
         public DataContext(DbContextOptions<DataContext> options) : base (options) {}
         
         public DbSet<Player> Players {get; set;}
+
+        public DbSet<User> Users {get; set;}
     }
 }
