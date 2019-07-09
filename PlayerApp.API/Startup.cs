616ca6c89b078d3b60ade0234693093c6219599a
Dalton <phandb72@gmail.com>
2019-07-08 22:34:06 -0500

    Error Handling

--- PlayerApp.API/Startup.cs
+++ PlayerApp.API/Startup.cs
@@ -15,6 +15,10 @@ using PlayerApp.API.Data;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
+using System.Net;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using PlayerApp.API.Helpers;
 
 namespace PlayerApp.API
 {
@@ -56,6 +60,19 @@ namespace PlayerApp.API
             }
             else
             {
+                app.UseExceptionHandler(buider=> {
+                    buider.Run(async context => {
+                        context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+                        var error = context.Features.Get<IExceptionHandlerFeature>();
+                        if (error !=null)
+                        {
+                            context.Response.AddApplicationError(error.Error.Message);
+                            await context.Response.WriteAsync(error.Error.Message);
+                        }
+                        
+                    });
+                });
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 //app.UseHsts();
             }
