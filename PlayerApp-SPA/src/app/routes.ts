2a22f4c62db5a455ca400ef3cba93bfc0aa4042f
Dalton <phandb72@gmail.com>
2019-07-11 23:36:17 -0500

    Add routes and guards components

---
+++ PlayerApp-SPA/src/app/routes.ts
@@ -0,0 +1,26 @@
+import {Routes} from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { MessagesComponent } from './messages/messages.component';
+import { MemberListComponent } from './member-list/member-list.component';
+import { ListsComponent } from './lists/lists.component';
+import { AuthGuard } from './_guards/auth.guard';
+
+export const appRoutes: Routes = [
+    // The ordering of the routes is important
+    // Don't place '**' at the top because it always routes to home.
+    { path: '', component: HomeComponent},
+    {
+        path: '',
+        runGuardsAndResolvers: 'always',
+        canActivate: [AuthGuard],
+        children: [
+            { path: 'members', component: MemberListComponent},
+            { path: 'messages', component: MessagesComponent},
+            { path: 'lists', component: ListsComponent},
+        ]
+    },
+   // { path: 'members', component: MemberListComponent, canActivate: [AuthGuard]},
+   // { path: 'messages', component: MessagesComponent},
+   // { path: 'lists', component: ListsComponent},
+    { path: '**', redirectTo: '', pathMatch: 'full' },
+];
